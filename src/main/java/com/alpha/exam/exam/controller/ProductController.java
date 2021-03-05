69fdc00f0dc37936041f26c4020a5ce9ef453204
Amornthep (Byte) <asiris3@ap.xom.com>
2021-03-05 15:32:11 +0700

    NA [Byte] - add GetMapping for get api in controller

--- src/main/java/com/alpha/exam/exam/controller/ProductController.java
+++ src/main/java/com/alpha/exam/exam/controller/ProductController.java
@@ -5,17 +5,22 @@ import com.alpha.exam.exam.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/v1/product")
 public class ProductController {
 
   @Autowired
   private ProductService productService;
 
-  public ResponseEntity<List<ProductDTO>> getProducts(String continent) {
+  @GetMapping
+  public ResponseEntity<List<ProductDTO>> getProducts(@RequestParam String continent) {
 
     List<ProductDTO> products = productService.getProducts(continent);
 
